f971b001b2c218845be20b726088b3dcf821fc79
Isabelle <isabellemsmart@gmail.com>
2023-09-28 12:37:35 -0500

    Home button fixed in CreateLessonPlanController and added some fx:id to the other buttons. Also matched the size of all the windows

--- GymnasticsApp/src/main/java/edu/augustana/CardBrowserController.java
+++ GymnasticsApp/src/main/java/edu/augustana/CardBrowserController.java
@@ -9,8 +9,6 @@ import javafx.fxml.FXML;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class CardBrowserController {
--- GymnasticsApp/src/main/java/edu/augustana/CreateLessonPlanController.java
+++ GymnasticsApp/src/main/java/edu/augustana/CreateLessonPlanController.java
@@ -17,14 +17,17 @@ public class CreateLessonPlanController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
-    @FXML // fx:id="browseCardsBtn"
-    private Button browseCardsBtn; // Value injected by FXMLLoader
+    @FXML private Button browseCardsBtn; // Value injected by FXMLLoader
 
-    @FXML // fx:id="doneButton"
-    private Button doneButton; // Value injected by FXMLLoader
+    @FXML private Button doneButton; // Value injected by FXMLLoader
 
-    @FXML // fx:id="homeBtn"
-    private Button homeBtn; // Value injected by FXMLLoader
+    @FXML private Button homeBtn; // Value injected by FXMLLoader
+
+    @FXML private Button printButton; // Value injected by FXMLLoader
+
+    @FXML private Button saveButton; // Value injected by FXMLLoader
+
+    @FXML private Button titleChangeButton; // Value injected by FXMLLoader
 
     @FXML
     void browseCards(ActionEvent event) throws IOException {
@@ -36,10 +39,19 @@ public class CreateLessonPlanController {
         App.setRoot("home");
     }
 
+    @FXML
+    void setTitle(ActionEvent event) {
+
+    }
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert browseCardsBtn != null : "fx:id=\"browseCardsBtn\" was not injected: check your FXML file 'create_lesson_plan.fxml'.";
+        assert doneButton != null : "fx:id=\"doneButton\" was not injected: check your FXML file 'create_lesson_plan.fxml'.";
         assert homeBtn != null : "fx:id=\"homeBtn\" was not injected: check your FXML file 'create_lesson_plan.fxml'.";
+        assert printButton != null : "fx:id=\"printButton\" was not injected: check your FXML file 'create_lesson_plan.fxml'.";
+        assert saveButton != null : "fx:id=\"saveButton\" was not injected: check your FXML file 'create_lesson_plan.fxml'.";
+        assert titleChangeButton != null : "fx:id=\"titleChangeButton\" was not injected: check your FXML file 'create_lesson_plan.fxml'.";
 
     }
 
