3485b4d8e2b1b0e2b5932d1fbb57e76a2e9dabc0
Isabelle <isabellemsmart@gmail.com>
2023-09-30 22:15:25 -0500

    got the text field to go away to set the title and then worked on the change title button

--- GymnasticsApp/src/main/java/edu/augustana/CreateLessonPlanController.java
+++ GymnasticsApp/src/main/java/edu/augustana/CreateLessonPlanController.java
@@ -1,15 +1,17 @@
 package edu.augustana;
 
-
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class CreateLessonPlanController {
 
@@ -28,6 +30,9 @@ public class CreateLessonPlanController {
     @FXML private TextField titleField; // Value injected by FXMLLoader
     @FXML private VBox titleBox; // Value injected by FXMLLoader
 
+    private Label title = new Label();
+    private ListView<?> lessonPlan = new ListView<>();
+    private String getTitle;
 
     @FXML void browseCards(ActionEvent event) throws IOException {
         App.setRoot("card_browser");
@@ -38,9 +43,30 @@ public class CreateLessonPlanController {
     }
 
     @FXML void setTitle(ActionEvent event) {
+        //setting up the title
+        getTitle = titleField.getText();
+        title.setText(getTitle);
+        Font titleFont = Font.font("Times New Roman", FontWeight.BOLD, 35);
+        title.setFont(titleFont);
 
+        //removing the done button and text field
+        titleBox.getChildren().remove(doneButton);
+        titleBox.getChildren().remove(titleField);
+
+        //adding the title and then a place for the cards to go
+        titleBox.getChildren().add(title);
+        titleBox.getChildren().add(lessonPlan);
     }
 
+    //I don't know if this is messing up the lesson plan
+    @FXML void editTitle(ActionEvent event) {
+        titleBox.getChildren().remove(title);
+        titleBox.getChildren().remove(lessonPlan);
+
+        titleBox.getChildren().add(titleField);
+        titleField.setText(getTitle);
+        titleBox.getChildren().add(doneButton);
+    }
 
 }
 
