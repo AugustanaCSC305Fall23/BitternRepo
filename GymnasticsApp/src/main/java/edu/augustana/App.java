432612a4ae44601a90d935327c5d67ba6c3f8e04
Jessica Cotturone <jessicacotturone21@augustana.edu>
2023-10-07 20:26:50 -0500

    Made all of the windows wider

--- GymnasticsApp/src/main/java/edu/augustana/App.java
+++ GymnasticsApp/src/main/java/edu/augustana/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("home"), 1134, 808);
+        scene = new Scene(loadFXML("home"), 1500, 800);
         stage.setScene(scene);
         stage.show();
     }
