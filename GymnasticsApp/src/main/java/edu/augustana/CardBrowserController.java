f2d684a1122846688edb1fb134955052cda529dd
Greyson <greysonhansen21@augustana.edu>
2023-10-08 17:24:16 -0500

    Added fx:id for all buttons and checkboxes on checkboxes on the card browser

--- GymnasticsApp/src/main/java/edu/augustana/CardBrowserController.java
+++ GymnasticsApp/src/main/java/edu/augustana/CardBrowserController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 
 public class CardBrowserController {
 
@@ -21,11 +22,62 @@ public class CardBrowserController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="applyFiltersButton"
+    private Button applyFiltersButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="clearFiltersButton"
+    private Button clearFiltersButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="backToLessonPlanBtn"
+    private Button backToLessonPlanBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="beamCheckBox"
+    private CheckBox beamCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="boyCheckBox"
+    private CheckBox boyCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="easyCheckBox"
+    private CheckBox easyCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="floorCheckBox"
+    private CheckBox floorCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="girlCheckBox"
+    private CheckBox girlCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="hardCheckBox"
+    private CheckBox hardCheckBox; // Value injected by FXMLLoader
+
     @FXML // fx:id="homeButton"
     private Button homeButton; // Value injected by FXMLLoader
 
-    @FXML // fx:id="backToLessonPlanBtn"
-    private Button backToLessonPlanBtn;
+    @FXML // fx:id="horizontalBarsCheckBox"
+    private CheckBox horizontalBarsCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="mediumCheckBox"
+    private CheckBox mediumCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="neutralCheckBox"
+    private CheckBox neutralCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="parallelBarsCheckBox"
+    private CheckBox parallelBarsCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="pommelHorseCheckBox"
+    private CheckBox pommelHorseCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="ringsCheckBox"
+    private CheckBox ringsCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="strengthCheckBox"
+    private CheckBox strengthCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="unevenBarsCheckBox"
+    private CheckBox unevenBarsCheckBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="vaultCheckBox"
+    private CheckBox vaultCheckBox; // Value injected by FXMLLoader
 
     @FXML
     private void goToHome(ActionEvent event) throws IOException {
