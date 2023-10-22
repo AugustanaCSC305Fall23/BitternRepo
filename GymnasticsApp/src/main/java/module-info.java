129b1163e1447a281a0508fa408c1888d2ea3c13
Isabelle <isabellemsmart@gmail.com>
2023-10-21 19:41:39 -0500

    Put the openCSV library in our project

--- GymnasticsApp/src/main/java/module-info.java
+++ GymnasticsApp/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module edu.augustana {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.controlsfx.controls;
+    requires com.opencsv;
 
     opens edu.augustana to javafx.fxml;
     exports edu.augustana;
