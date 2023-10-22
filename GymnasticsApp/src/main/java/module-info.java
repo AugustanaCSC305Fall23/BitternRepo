5883ac11213f8b589c58450dec3b3f0f4a114255
Isabelle <isabellemsmart@gmail.com>
2023-10-22 17:12:57 -0500

    Revert "Put the openCSV library in our project"
    This reverts commit 129b1163e1447a281a0508fa408c1888d2ea3c13.

--- GymnasticsApp/src/main/java/module-info.java
+++ GymnasticsApp/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module edu.augustana {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.controlsfx.controls;
-    requires com.opencsv;
 
     opens edu.augustana to javafx.fxml;
     exports edu.augustana;
