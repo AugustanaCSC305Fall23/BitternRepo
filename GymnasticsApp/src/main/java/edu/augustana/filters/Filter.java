9bc059bbd78e02c4c44a64593ae151ea62a9366f
Jessica Cotturone <jessicacotturone21@augustana.edu>
2023-11-30 12:03:54 -0600

    Changed the color of the buttons in the stylesheet and refactored class name Filters to Filter

--- GymnasticsApp/src/main/java/edu/augustana/filters/Filter.java
+++ GymnasticsApp/src/main/java/edu/augustana/filters/Filter.java
@@ -2,7 +2,7 @@ package edu.augustana.filters;
 
 import edu.augustana.model.Card;
 
-public interface Filters {
+public interface Filter {
     public boolean matchesFilters(Card card);
     public void resetDesiredFiltersList();
 }
