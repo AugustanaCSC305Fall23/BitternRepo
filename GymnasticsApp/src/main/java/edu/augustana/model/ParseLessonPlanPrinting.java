eb633cbd8b21bf61ea349d95c1cc27be01767096
Lucas Clayton <lucasclayton22@augustana.edu>
2023-12-09 11:40:59 -0600

    Refactored ParseLessonPlanPrinting and added JavaDoc comments to ParseLessonPlanPrinting

--- GymnasticsApp/src/main/java/edu/augustana/model/ParseLessonPlanPrinting.java
+++ GymnasticsApp/src/main/java/edu/augustana/model/ParseLessonPlanPrinting.java
@@ -47,19 +47,25 @@ public class ParseLessonPlanPrinting {
 
     private Font eventTitleTemplate = new Font("Times New Roman Bold", 15);
 
+    double landscapeHeightScale = 1.25;
+
+    double portraitHeightScale = 1.75;
+
 
     // ---------- FXML ----------
     @FXML
     private Label lessonPlanTitleLabel = new Label();
 
-    @FXML
-    private FlowPane eventCardsTemplate = new FlowPane();
-    @FXML
-    private CardView cardTemplate;
 
-
-    // The goal of this class is to set up the layout of each page and to set up the
-    // list of Nodes that will represent the pages of the lesson plan
+    /**
+     * Creates an instance of the ParseLessonPlanPrininting Class. This class utillizes info from
+     * the PrintStaging class to parse through the information and crate the pages for printing and the
+     * print preview.
+     *
+     * @param printerJob - the PrinterJob that representsthe printer
+     * @param is_printing - true if the Parsing is for actual printing (not the preview displayed on screen).
+     * @throws MalformedURLException
+     */
     public ParseLessonPlanPrinting(PrinterJob printerJob, boolean is_printing) throws MalformedURLException {
 
         isPrinting = is_printing;
@@ -72,19 +78,17 @@ public class ParseLessonPlanPrinting {
                 pageHeight = pgLayout.getPrintableWidth();
                 pageWidth = pgLayout.getPrintableHeight();
             } else {
-                pageHeight = pgLayout.getPrintableWidth() * 1.25;
+                pageHeight = pgLayout.getPrintableWidth() * landscapeHeightScale;
                 pageWidth = pgLayout.getPrintableHeight() * 1.75;
                 eventTitleTemplate = new Font("Times New Roman Bold", 20);
                 titleFont = new Font("Times New Roman Bold", 35);
             }
         }  else {
-            System.out.println(isPrinting);
-
             if (isPrinting) {
                 pageHeight = pgLayout.getPrintableHeight();
                 pageWidth = pgLayout.getPrintableWidth();
             } else {
-                pageHeight = pgLayout.getPrintableHeight() * 1.75;
+                pageHeight = pgLayout.getPrintableHeight() * portraitHeightScale;
                 pageWidth = pgLayout.getPrintableWidth() * 1.75;
                 eventTitleTemplate = new Font("Times New Roman Bold", 20);
                 titleFont = new Font("Times New Roman Bold", 35);
@@ -98,6 +102,13 @@ public class ParseLessonPlanPrinting {
         }
     }
 
+    /**
+     * Parses through the lesson plan and creates the pages for the landscape and portrait version of a lesson plan
+     * with cards.
+     *
+     * @param pgLayout - The PageLayout of the given printer.
+     * @throws MalformedURLException
+     */
     private void parseLessonPlanImages(PageLayout pgLayout) throws MalformedURLException {
         labelToFlowPaneMap = new LinkedHashMap<Label, FlowPane>();
         eventToCardsMap = PrintStaging.getEventToCardMap();
@@ -108,8 +119,6 @@ public class ParseLessonPlanPrinting {
         // Used https://codegym.cc/groups/posts/how-to-iterate-a-map-in-java to run through a Map
         initializeLabelToFlowPaneMap();
 
-
-// Sets up pages
         // Sets up dummy scene so that we can render the actual height/width of the JavaFX Objects
         // Used https://stackoverflow.com/questions/26152642/get-the-height-of-a-node-in-javafx-generate-a-layout-pass
         double runningHeight = 0;
@@ -142,7 +151,24 @@ public class ParseLessonPlanPrinting {
         double runningPageHeight = lessonPlanTitleLabel.getHeight();
 
 
-    // Runs through the JavaFX Objects map to add the given events and cards to the page
+        // Runs through the JavaFX Objects map to add the given events and cards to the page
+        addCardImageToPage(dummyBox, dummyRoot, runningPageHeight, pageContents, currentPane);
+
+        TextFlow equipmentTF = new TextFlow();
+        // Adds equipment
+        if (PrintStaging.getEquipmentDisplay()) {
+            if (PrintStaging.getLandscapeDisplay()) {
+                addEquipment(landscapeHeightScale, equipmentTF, dummyBox, dummyRoot, currentPane, pageContents);
+            } else {
+                addEquipment(portraitHeightScale, equipmentTF, dummyBox, dummyRoot, currentPane, pageContents);
+            }
+        }
+
+
+
+    }
+
+    private void addCardImageToPage(VBox dummyBox, Group dummyRoot, double runningPageHeight, VBox pageContents, Pane currentPane) {
         for (Map.Entry<Label, FlowPane> event : labelToFlowPaneMap.entrySet()) {
 
             Label eventLabel = event.getKey();
@@ -180,79 +206,10 @@ public class ParseLessonPlanPrinting {
         }
 
         pages.add(currentPane);
-
-        // Adds equipment
-        TextFlow equipmentTF = new TextFlow();
-
-        if (PrintStaging.getEquipmentDisplay()) {
-            List<String> equipmentList = App.getCurrentLessonPlan().getEquipmentFromMap(eventToCardsMap);
-            if (equipmentList != null && (!(equipmentList.isEmpty()))) {
-                String equipmentSentence = "EQUIPMENT: \n" + equipmentList.get(0);
-                String equipmentString = "Equipment: \n" + "   *" + equipmentList.get(0);
-                for (int x = 1; x < equipmentList.size(); x++) {
-                    equipmentString = equipmentString + "\n   *" + equipmentList.get(x);
-                    equipmentSentence = equipmentSentence + ", " + equipmentList.get(x);
-                }
-
-                Text equipmentText = new Text(equipmentString);
-                Font font = new Font("Times New Roman", 15);
-                equipmentText.setFont(font);
-                equipmentTF = new TextFlow(equipmentText);
-                equipmentTF.setMaxWidth(pageWidth);
-
-                dummyBox = new VBox();
-                dummyRoot = new Group();
-                dummyBox.getChildren().add(equipmentTF);
-                dummyRoot.getChildren().add(dummyBox);
-                dummyRoot.applyCss();
-                dummyRoot.layout();
-
-                if (!(dummyBox.getHeight() > pageHeight)) {
-
-                    currentPane = new Pane();
-                    currentPane.setMaxHeight(pageHeight);
-                    currentPane.setMaxWidth(pageWidth);
-
-                    pageContents = new VBox();
-                    pageContents.setAlignment(Pos.CENTER);
-                    pageContents.setMaxHeight(pageHeight);
-                    currentPane.getChildren().add(pageContents);
-
-                    pageContents.getChildren().add(equipmentTF);
-
-                    pages.add(currentPane);
-                } else {
-                    equipmentText = new Text(equipmentSentence);
-                    font = new Font("Times New Roman", 15);
-                    equipmentText.setFont(font);
-                    equipmentTF = new TextFlow(equipmentText);
-                    equipmentTF.setMaxWidth(pageWidth);
-
-                    currentPane = new Pane();
-                    currentPane.setMaxHeight(pageHeight);
-                    currentPane.setMaxWidth(pageWidth);
-
-                    pageContents = new VBox();
-                    pageContents.setAlignment(Pos.CENTER);
-                    pageContents.setMaxHeight(pageHeight);
-                    currentPane.getChildren().add(pageContents);
-
-                    pageContents.getChildren().add(equipmentTF);
-
-                    pages.add(currentPane);
-                }
-            }
-
-
-        }
-
-
-
     }
 
     private void initializeLabelToFlowPaneMap() throws MalformedURLException {
         for (Map.Entry<String, List<Card>> event : eventToCardsMap.entrySet()) {
-            // Set up Label to card FlowPane Map
             Label eventLabel = new Label();
             eventLabel.setFont(eventTitleTemplate);
             eventLabel.setText(event.getKey());
@@ -265,8 +222,6 @@ public class ParseLessonPlanPrinting {
                 ImageView cardImageView = new ImageView(card.getImage());
                 if (numCards > 12) {
                     // Used https://stackoverflow.com/questions/7446710/how-to-round-up-integer-division-and-have-int-result-in-java
-
-
                     int numRows;
                     if (PrintStaging.getLandscapeDisplay()) {
                         numRows = (int) Math.ceil(numCards / 4.0);
@@ -308,6 +263,12 @@ public class ParseLessonPlanPrinting {
         }
     }
 
+    /**
+     * Parses through the lesson plan and creates the pages for the text only version of a lesson plan.
+     *
+     * @param pgLayout - The PageLayout of the given printer.
+     * @throws MalformedURLException
+     */
     private void parseLessonPlanText(PageLayout pgLayout) throws MalformedURLException {
         labelToCardLabelsMap = new LinkedHashMap<Label, List<Label>>();
         eventToCardsMap = PrintStaging.getEventToCardMap();
@@ -317,7 +278,6 @@ public class ParseLessonPlanPrinting {
         // Used https://codegym.cc/groups/posts/how-to-iterate-a-map-in-java to run through a Map
         initializeLabelToCardLabelsMap();
 
-// Sets up pages
         // Sets up dummy scene so that we can render the actual height/width of the JavaFX Objects
         // Used https://stackoverflow.com/questions/26152642/get-the-height-of-a-node-in-javafx-generate-a-layout-pass
         Group dummyRoot = new Group();
@@ -349,6 +309,106 @@ public class ParseLessonPlanPrinting {
 
 
         // Runs through the JavaFX Objects map to add the given events and cards to the page
+        addTextToPage(dummyBox, dummyRoot, runningPageHeight, pageContents, currentPane);
+
+
+        TextFlow equipmentTF = new TextFlow();
+        // Adds equipment
+        if (PrintStaging.getEquipmentDisplay()) {
+            addEquipment(portraitHeightScale, equipmentTF, dummyBox, dummyRoot, currentPane, pageContents);
+        }
+    }
+
+    private void initializeLabelToCardLabelsMap() {
+
+        for (Map.Entry<String, List<Card>> event : eventToCardsMap.entrySet()) {
+            double runningHeight = 0;
+
+            Label eventLabel = new Label();
+            eventLabel.setFont(eventTitleTemplate);
+            eventLabel.setText(event.getKey());
+            eventLabel.setPadding(new Insets(10,0, 10, 0));
+            List<Label> eventCards = new ArrayList<>();
+            for (Card card : event.getValue()) {
+                Label cardInfo = new Label();
+                String cardInfoS = "-   " + card.getTitle() +", " + card.getCode();
+                cardInfo.setText(cardInfoS);
+                if (!isPrinting) {
+                    cardInfo.setFont(new Font("Times New Roman", 15 * 1.75));
+                }
+                eventCards.add(cardInfo);
+            }
+
+            if (!isPrinting) {
+                eventLabel.setFont(new Font("Times New Roman", 20 * 1.75));
+            }
+            labelToCardLabelsMap.put(eventLabel, eventCards);
+        }
+    }
+
+    private void addEquipment(double equipmentHeightScale, TextFlow equipmentTF, VBox dummyBox, Group dummyRoot, Pane currentPane, VBox pageContents) {
+        List<String> equipmentList = App.getCurrentLessonPlan().getEquipmentFromMap(eventToCardsMap);
+        if (equipmentList != null && (!(equipmentList.isEmpty()))) {
+            String equipmentSentence = "EQUIPMENT: \n" + equipmentList.get(0);
+            String equipmentString = "Equipment: \n" + "   *" + equipmentList.get(0);
+            for (int x = 1; x < equipmentList.size(); x++) {
+                equipmentString = equipmentString + "\n   *" + equipmentList.get(x);
+                equipmentSentence = equipmentSentence + ", " + equipmentList.get(x);
+            }
+
+            Text equipmentText = new Text(equipmentString);
+            Font font = new Font("Times New Roman", 15 * equipmentHeightScale);
+            if (isPrinting) {
+                font = new Font("Times New Roman", 15 );
+            }
+            equipmentText.setFont(font);
+            equipmentTF = new TextFlow(equipmentText);
+            equipmentTF.setMaxWidth(pageWidth);
+
+            dummyBox = new VBox();
+            dummyRoot = new Group();
+            dummyBox.getChildren().add(equipmentTF);
+            dummyRoot.getChildren().add(dummyBox);
+            dummyRoot.applyCss();
+            dummyRoot.layout();
+
+            if (!(dummyBox.getHeight() > pageHeight)) {
+
+                currentPane = new Pane();
+                currentPane.setMaxHeight(pageHeight);
+                currentPane.setMaxWidth(pageWidth);
+
+                pageContents = new VBox();
+                pageContents.setAlignment(Pos.CENTER);
+                pageContents.setMaxHeight(pageHeight);
+                currentPane.getChildren().add(pageContents);
+
+                pageContents.getChildren().add(equipmentTF);
+
+                pages.add(currentPane);
+            } else {
+                equipmentText = new Text(equipmentSentence);
+                equipmentText.setFont(font);
+                equipmentTF = new TextFlow(equipmentText);
+                equipmentTF.setMaxWidth(pageWidth);
+
+                currentPane = new Pane();
+                currentPane.setMaxHeight(pageHeight);
+                currentPane.setMaxWidth(pageWidth);
+
+                pageContents = new VBox();
+                pageContents.setAlignment(Pos.CENTER);
+                pageContents.setMaxHeight(pageHeight);
+                currentPane.getChildren().add(pageContents);
+
+                pageContents.getChildren().add(equipmentTF);
+
+                pages.add(currentPane);
+            }
+        }
+    }
+
+    private void addTextToPage(VBox dummyBox, Group dummyRoot, double runningPageHeight, VBox pageContents, Pane currentPane) {
         for (Map.Entry<Label, List<Label>> event : labelToCardLabelsMap.entrySet()) {
 
             Label eventLabel = event.getKey();
@@ -401,99 +461,6 @@ public class ParseLessonPlanPrinting {
         }
 
         pages.add(currentPane);
-
-        // Adds equipment
-        TextFlow equipmentTF = new TextFlow();
-
-        if (PrintStaging.getEquipmentDisplay()) {
-            List<String> equipmentList = App.getCurrentLessonPlan().getEquipmentFromMap(eventToCardsMap);
-            if (equipmentList != null && (!(equipmentList.isEmpty()))) {
-                String equipmentSentence = "EQUIPMENT: \n" + equipmentList.get(0);
-                String equipmentString = "Equipment: \n" + "   *" + equipmentList.get(0);
-                for (int x = 1; x < equipmentList.size(); x++) {
-                    equipmentString = equipmentString + "\n   *" + equipmentList.get(x);
-                    equipmentSentence = equipmentSentence + ", " + equipmentList.get(x);
-                }
-
-                Text equipmentText = new Text(equipmentString);
-                Font font = new Font("Times New Roman", 15 * 1.75);
-                if (isPrinting) {
-                    font = new Font("Times New Roman", 15 );
-                }
-                equipmentText.setFont(font);
-                equipmentTF = new TextFlow(equipmentText);
-                equipmentTF.setMaxWidth(pageWidth);
-
-                dummyBox = new VBox();
-                dummyRoot = new Group();
-                dummyBox.getChildren().add(equipmentTF);
-                dummyRoot.getChildren().add(dummyBox);
-                dummyRoot.applyCss();
-                dummyRoot.layout();
-
-                if (!(dummyBox.getHeight() > pageHeight)) {
-
-                    currentPane = new Pane();
-                    currentPane.setMaxHeight(pageHeight);
-                    currentPane.setMaxWidth(pageWidth);
-
-                    pageContents = new VBox();
-                    pageContents.setAlignment(Pos.CENTER);
-                    pageContents.setMaxHeight(pageHeight);
-                    currentPane.getChildren().add(pageContents);
-
-                    pageContents.getChildren().add(equipmentTF);
-
-                    pages.add(currentPane);
-                } else {
-                    equipmentText = new Text(equipmentSentence);
-                    equipmentText.setFont(font);
-                    equipmentTF = new TextFlow(equipmentText);
-                    equipmentTF.setMaxWidth(pageWidth);
-
-                    currentPane = new Pane();
-                    currentPane.setMaxHeight(pageHeight);
-                    currentPane.setMaxWidth(pageWidth);
-
-                    pageContents = new VBox();
-                    pageContents.setAlignment(Pos.CENTER);
-                    pageContents.setMaxHeight(pageHeight);
-                    currentPane.getChildren().add(pageContents);
-
-                    pageContents.getChildren().add(equipmentTF);
-
-                    pages.add(currentPane);
-                }
-
-            }
-        }
-    }
-
-    private void initializeLabelToCardLabelsMap() {
-
-        for (Map.Entry<String, List<Card>> event : eventToCardsMap.entrySet()) {
-            double runningHeight = 0;
-
-            Label eventLabel = new Label();
-            eventLabel.setFont(eventTitleTemplate);
-            eventLabel.setText(event.getKey());
-            eventLabel.setPadding(new Insets(10,0, 10, 0));
-            List<Label> eventCards = new ArrayList<>();
-            for (Card card : event.getValue()) {
-                Label cardInfo = new Label();
-                String cardInfoS = "-   " + card.getTitle() +", " + card.getCode();
-                cardInfo.setText(cardInfoS);
-                if (!isPrinting) {
-                    cardInfo.setFont(new Font("Times New Roman", 15 * 1.75));
-                }
-                eventCards.add(cardInfo);
-            }
-
-            if (!isPrinting) {
-                eventLabel.setFont(new Font("Times New Roman", 20 * 1.75));
-            }
-            labelToCardLabelsMap.put(eventLabel, eventCards);
-        }
     }
 
     private void initializeTitle() {
@@ -503,7 +470,11 @@ public class ParseLessonPlanPrinting {
     }
 
 
-
+    /**
+     * Returns the pages of the lesson plan.
+     *
+     * @return a List of pages, which represents the Nodes on which the Lesson Plan is displayed/printed.
+     */
     public ArrayList<Pane> getPages() {
         return pages;
     }
